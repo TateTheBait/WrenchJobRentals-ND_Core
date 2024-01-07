aaa3a13e761c45fb3cc3373170fa351e6afe304a
TateTheBait <123601564+TateTheBait@users.noreply.github.com>
2024-01-07 13:56:57 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -10,11 +10,12 @@ Config = {
                 price = Carprice, -- this can be changed independently it just has to be a number, this is for ease of access
                 hash = `police1`,
                 vehicleextras = {e1 = true, e2 = false, e3 = false, e4 = false, e5 = true, e6 = false, e7 = false, e8 = false, e9 = false, e10 = false, e11 = false, e12 = false, e13 = false, e14 = false}, -- YOU DONT HAVE TO ADD THIS | DELETE THIS LINE IF YOU WANT NO EXTRAS!!! | IF YOU DO HAVE THIS LINE SET ALL 1-14 DO NOT DELETE ANY!!!
-            },
+                livery = 2 -- OPTIONAL
             {
                 name = "Police2",
                 price = Carprice, -- this can be changed independently it just has to be a number, this is for ease of access
-                hash = `police2`
+                hash = `police2`,
+                livery = 2 -- OPTIONAL
             },
             {
                 name = "Police3",
