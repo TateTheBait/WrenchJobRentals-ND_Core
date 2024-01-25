61f923f0c0ae81da6dca683c9c245502fb392b17
TateTheBait <123601564+TateTheBait@users.noreply.github.com>
2024-01-24 19:40:49 -0600

    Added ranks

--- config.lua
+++ config.lua
@@ -5,24 +5,26 @@ Config = {
     cars = {
         ["POLICE"] = {
             hasrun = false, -- DO NOT CHANGE
-            {
+            { -- this car has ranks and extras and livery as an example
                 name = "Police1",
                 price = Carprice, -- this can be changed independently it just has to be a number, this is for ease of access
                 hash = `police1`,
                 vehicleextras = {e1 = true, e2 = false, e3 = false, e4 = false, e5 = true, e6 = false, e7 = false, e8 = false, e9 = false, e10 = false, e11 = false, e12 = false, e13 = false, e14 = false}, -- YOU DONT HAVE TO ADD THIS | DELETE THIS LINE IF YOU WANT NO EXTRAS!!! | IF YOU DO HAVE THIS LINE SET ALL 1-14 DO NOT DELETE ANY!!!
-                livery = 2 -- OPTIONAL
+                livery = 2, -- OPTIONAL
+                ranks = {""} -- OPTIONAL | CAN BE REMOVED OR ADDED | USES THE RANK NAME, NOT THE NUMBER!!!!!!!!!
             },
-            {
+            { -- This car has ranks removed as an example
                 name = "Police2",
                 price = Carprice, -- this can be changed independently it just has to be a number, this is for ease of access
                 hash = `police2`,
-                livery = 2 -- OPTIONAL
+                livery = 2, -- OPTIONAL
             },
-            {
+            { -- this car has extras and ranks as an example
                 name = "Police3",
                 price = Carprice, -- this can be changed independently it just has to be a number, this is for ease of access
                 hash = `police3`,
                 vehicleextras = {e1 = true, e2 = false, e3 = false, e4 = false, e5 = true, e6 = false, e7 = false, e8 = false, e9 = false, e10 = false, e11 = false, e12 = false, e13 = false, e14 = false}, -- YOU DONT HAVE TO ADD THIS | DELETE THIS LINE IF YOU WANT NO EXTRAS!!! | IF YOU DO HAVE THIS LINE SET ALL 1-14 DO NOT DELETE ANY!!!
+                ranks = {""} -- OPTIONAL | CAN BE REMOVED OR ADDED | USES THE RANK NAME, NOT THE NUMBER!!!!!!!!!
             },
         },
     },
