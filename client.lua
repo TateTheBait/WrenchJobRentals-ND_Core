b1686058f318459b53bb5b18cb943ca907cb3c0a
TateTheBait <123601564+TateTheBait@users.noreply.github.com>
2024-03-12 18:52:22 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -3,8 +3,7 @@ local rentedcars = {}
 local Peds = {}
 
 
-RegisterNetEvent("poor")
-AddEventHandler("poor", function ()
+RegisterNetEvent("poor", function ()
     lib.notify({
         title = "Wrench Leo Rental",
         description = "You don't have the required amount of money to rent a this vehicle!",
@@ -13,52 +12,47 @@ AddEventHandler("poor", function ()
 end)
 
 
-RegisterNetEvent("carRental:confirm")
-AddEventHandler("carRental:confirm", function(table)
-        local veh = 0
-        local car = table.car
-        SelectedCar = table.car
-        RequestModel(car.hash)
-        while not HasModelLoaded(car.hash) do
-            Wait(10)
+RegisterNetEvent("carRental:confirm", function(table)
+    local veh = 0
+    local car = table.car
+    SelectedCar = table.car
+    lib.requestModel(car.hash)
+    veh = CreateVehicle(car.hash, table.location.x, table.location.y, table.location.z, table.location.w, true, false)
+    if car.vehicleextras then
+        for eid, extra in pairs(car.vehicleextras) do
+            SetVehicleExtra(veh, eid, extra)
         end
-        veh = CreateVehicle(car.hash, table.location.x, table.location.y, table.location.z, table.location.w, true, false)
-        if car.vehicleextras then
-            for eid, extra in pairs(car.vehicleextras) do
-                SetVehicleExtra(veh, eid, extra)
-            end
-        else
-            for num=1, 14 do 
-                SetVehicleExtra(veh, num , 1)
-            end
+    else
+        for num=1, 14 do 
+            SetVehicleExtra(veh, num , 1)
         end
-        if car.livery then
-            SetVehicleLivery(veh, car.livery)
-        end
-        VEHICLE = veh
-        SetVehicleEngineOn(veh, true, true, false)
-        if DoesEntityExist(veh) then     
-            SetVehicleDirtLevel(veh, 0.00)
-            lib.notify({
-                title = "Wrench Leo Rental",
-                description = "Successfully rented vehicle for $" .. tostring(car.price) .. "!",
-                icon = "hand-fist",
-            })
-            local netid = NetworkGetNetworkIdFromEntity(veh)
-            Netid = netid
-            local tbl = {
-                veh = netid,
-                src = cache.serverId,
-            }
-            Wait(200)
-            TriggerServerEvent("Sold", tbl)
-            lib.hideTextUI()
-            rentedcars.vehicle = veh
-        end
+    end
+    if car.livery then
+        SetVehicleLivery(veh, car.livery)
+    end
+    VEHICLE = veh
+    SetVehicleEngineOn(veh, true, true, false)
+    if DoesEntityExist(veh) then     
+        SetVehicleDirtLevel(veh, 0.00)
+        lib.notify({
+            title = "Wrench Leo Rental",
+            description = "Successfully rented vehicle for $" .. tostring(car.price) .. "!",
+            icon = "hand-fist",
+        })
+        local netid = NetworkGetNetworkIdFromEntity(veh)
+        Netid = netid
+        local tbl = {
+            veh = netid,
+            src = cache.serverId,
+        }
+        Wait(200)
+        TriggerServerEvent("Sold", tbl)
+        lib.hideTextUI()
+        rentedcars.vehicle = veh
+    end
 end)
 
-RegisterNetEvent("carRental:deny")
-AddEventHandler("carRental:deny", function()
+RegisterNetEvent("carRental:deny", function()
     if not rentedcars.vehicle then return end
     local cat = Config.cars[Category]
     local car = cat[SelectedCar]
@@ -76,40 +70,41 @@ AddEventHandler("carRental:deny", function()
 end)
 
 -- Rentals
-function getcars(location)
+local function getcars(location)
     local cars = {}
     local cartable = {}
     Locationtbl = {}
-    for _, job in pairs(location.jobs) do
-        if tostring(NDCore.getPlayer().job) == tostring(job) then
-            for _, i in pairs(location.categories) do
-                for _, car in pairs(Config.cars[i]) do
-                        if car.ranks then
-                            for _, rank in pairs(car.ranks) do
-                                if not car.hasrun then
-                                    car.hasrun = true
-                                    if NDCore.getPlayer().job.rankName == rank then
+    if location.jobs then
+        for _, job in pairs(location.jobs) do
+            if tostring(NDCore.getPlayer().job) == tostring(job) then
+                for _, i in pairs(location.categories) do
+                    for _, car in pairs(Config.cars[i]) do
+                            if car.ranks then
+                                for _, rank in pairs(car.ranks) do
+                                    if not car.hasrun then
+                                        car.hasrun = true
+                                        if NDCore.getPlayer().job.rankName == rank then
+                                            car.hasrun = true
+                                            cartable[#cartable + 1] = (car)
+                                        end
+                                    end
+                                end
+                            else
+                                for _, car in pairs(Config.cars[i]) do
+                                    if not car.hasrun then
                                         car.hasrun = true
                                         cartable[#cartable + 1] = (car)
                                     end
                                 end
                             end
-                        else
+                        end
+                        for _, i in pairs(location.categories) do
                             for _, car in pairs(Config.cars[i]) do
-                                if not car.hasrun then
-                                    car.hasrun = true
-                                    cartable[#cartable + 1] = (car)
-                                end
+                                car.hasrun = nil
                             end
                         end
-                    end
-                    for _, i in pairs(location.categories) do
-                        for _, car in pairs(Config.cars[i]) do
-                            car.hasrun = nil
-                        end
-                    end
-               
-                for id, car in pairs(cartable) do
+                
+                    for id, car in pairs(cartable) do
                         cars[#cars + 1] = {
                             title = car.name,
                             onSelect = function()
@@ -124,7 +119,53 @@ function getcars(location)
                                 {label = 'Deposit', value = car.price},
                             }
                         }
+                    end
                 end
+            end
+        end
+    else
+        for _, i in pairs(location.categories) do
+            for _, car in pairs(Config.cars[i]) do
+                    if car.ranks then
+                        for _, rank in pairs(car.ranks) do
+                            if not car.hasrun then
+                                car.hasrun = true
+                                if NDCore.getPlayer().job.rankName == rank then
+                                    car.hasrun = true
+                                    cartable[#cartable + 1] = (car)
+                                end
+                            end
+                        end
+                    else
+                        for _, car in pairs(Config.cars[i]) do
+                            if not car.hasrun then
+                                car.hasrun = true
+                                cartable[#cartable + 1] = (car)
+                            end
+                        end
+                    end
+                end
+                for _, i in pairs(location.categories) do
+                    for _, car in pairs(Config.cars[i]) do
+                        car.hasrun = nil
+                    end
+                end
+        
+            for id, car in pairs(cartable) do
+                    cars[#cars + 1] = {
+                        title = car.name,
+                        onSelect = function()
+                            local tbl = {
+                                location = location.vehspawnlocation,
+                                car = Config.cars[i][id],
+                                plrid = cache.serverId
+                            }
+                            TriggerServerEvent("carRental:pay", id, tbl)
+                        end,
+                        metadata = {
+                            {label = 'Deposit', value = car.price},
+                        }
+                    }
             end
         end
     end
@@ -136,33 +177,33 @@ end
 RETURNVEHICLE = {
     label = "Return Last Vehicle",
     onSelect = function()
-            if rentedcars.vehicle then
-                if GetVehicleBodyHealth(VEHICLE) >= 300 and DoesEntityExist(VEHICLE) then
-                    TriggerServerEvent("Returned", cache.serverId, SelectedCar.price, rentedcars.vehicle, true, Netid)
-                    lib.notify({
-                        title = "Wrench Leo Rental",
-                        description = "Thank you for returning your vehicle.",
-                        icon = "hand-fist",
-                    })
-                else
-                    TriggerServerEvent("Returned", cache.serverId, SelectedCar.price, rentedcars.vehicle, false, Netid)
-                    lib.notify({
-                        title = "Wrench Leo Rental",
-                        description = "Your car is severely damaged or has been destroyed, please repair it before returning next time!",
-                        icon = "hand-fist",
-                    })
-                end
-                rentedcars = {}
-            else
+        if rentedcars.vehicle then
+            if GetVehicleBodyHealth(VEHICLE) >= 300 and DoesEntityExist(VEHICLE) then
+                TriggerServerEvent("Returned", cache.serverId, SelectedCar.price, rentedcars.vehicle, true, Netid)
                 lib.notify({
                     title = "Wrench Leo Rental",
-                    description = "You haven't rented a vehicle yet!!!",
+                    description = "Thank you for returning your vehicle.",
                     icon = "hand-fist",
                 })
-          end
-        end,
-        icon = "caret-right"
-  }
+            else
+                TriggerServerEvent("Returned", cache.serverId, SelectedCar.price, rentedcars.vehicle, false, Netid)
+                lib.notify({
+                    title = "Wrench Leo Rental",
+                    description = "Your car is severely damaged or has been destroyed, please repair it before returning next time!",
+                    icon = "hand-fist",
+                })
+            end
+            rentedcars = {}
+        else
+            lib.notify({
+                title = "Wrench Leo Rental",
+                description = "You haven't rented a vehicle yet!!!",
+                icon = "hand-fist",
+            })
+        end
+    end,
+    icon = "caret-right"
+}
 
 function peds()
     for _, ped in pairs(Peds) do
@@ -170,35 +211,71 @@ function peds()
     end
     for _, location in pairs(Config.locations) do
         local isgood = false
-        for _, job in pairs(location.jobs) do
-            if NDCore.getPlayer().job == tostring(job) then
-                isgood = true
+        if location.jobs then
+            for _, job in pairs(location.jobs) do
+                if NDCore.getPlayer().job == tostring(job) then
+                    isgood = true
+                end
             end
+                if isgood == true then
+                    local model = lib.requestModel(location.pedhash)
+                    local ped = NDCore.createAiPed({
+                        model = model, 
+                        coords = vector4(location.pedlocation.x, location.pedlocation.y, location.pedlocation.z-1, location.pedlocation.w),
+                        blip = {
+                            sprite = 672,
+                            color = 15, 
+                            scale = 1,
+                            label = location.name .. " Vehicle Rental"
+                        },
+                        anim = {
+                            clip = "WORLD_HUMAN_COP_IDLES"
+                        }, 
+                        options = {
+                            {
+                                label = "Open Menu",
+                                onSelect = function()
+                                    lib.showContext(tostring(location.name) .. '_menu')
+                                end,
+                                icon = "car-rear"
+                            }, 
+                            RETURNVEHICLE
+                        }
+                    })
+                    FreezeEntityPosition(ped, true)
+                    SetEntityInvincible(ped, true)
+                    SetBlockingOfNonTemporaryEvents(ped, true)
+                    TaskStartScenarioInPlace(ped, "WORLD_HUMAN_COP_IDLES", 0, true)
+                    Peds[#Peds+1] = ped
+            end
+        else
+            local model = lib.requestModel(location.pedhash)
+            local ped = CreatePed(4, model, location.pedlocation.x, location.pedlocation.y, location.pedlocation.z-1, location.pedlocation.w, false, false)
+            FreezeEntityPosition(ped, true)
+            SetEntityInvincible(created_ped, true)
+            SetBlockingOfNonTemporaryEvents(ped, true)
+            TaskStartScenarioInPlace(ped, "WORLD_HUMAN_COP_IDLES", 0, true)
+            local options2 = {
+                label = "Open Menu",
+                onSelect = function()
+                    lib.showContext(tostring(location.name) .. '_menu')
+                end,
+                icon = "car-rear"
+            }
+            exports.ox_target:addLocalEntity(ped, options2)
+            exports.ox_target:addLocalEntity(ped, RETURNVEHICLE)
+            
+            Peds[#Peds+1] = ped
         end
-            if isgood == true then
-                local model = lib.requestModel(location.pedhash)
-                local ped = CreatePed(4, model, location.pedlocation.x, location.pedlocation.y, location.pedlocation.z-1, location.pedlocation.w, false, false)
-                FreezeEntityPosition(ped, true)
-                SetEntityInvincible(created_ped, true)
-                SetBlockingOfNonTemporaryEvents(ped, true)
-                TaskStartScenarioInPlace(ped, "WORLD_HUMAN_COP_IDLES", 0, true)
-                local optio = {
-                    label = "Open Menu",
-                    onSelect = function()
-                        lib.showContext(tostring(location.name) .. '_menu')
-                    end,
-                    icon = "car-rear"
-                  }
-                exports.ox_target:addLocalEntity(ped, optio)
-                exports.ox_target:addLocalEntity(ped, RETURNVEHICLE)
-                
-                Peds[#Peds+1] = ped
-         end
     end
 end
 
-RegisterNetEvent("ND:characterLoaded")
-AddEventHandler("ND:characterLoaded", function()
+RegisterNetEvent("ND:characterLoaded", function ()
+    local player = NDCore.getPlayer()
+    lib.notify({
+        label = "Job Rental",
+        description = tostring(player.job) .. " Character Registered!"
+    })
     for _, location in pairs(Config.locations) do
         location.menu = lib.registerContext({
             id = tostring(location.name) .. '_menu',
@@ -208,3 +285,20 @@ AddEventHandler("ND:characterLoaded", function()
     end
     peds()
 end)
+
+
+if NDCore.getPlayer() ~= nil then
+    local player = NDCore.getPlayer()
+    lib.notify({
+        label = "Job Rental",
+        description = tostring(player.job) .. " Character Registered!"
+    })
+    for _, location in pairs(Config.locations) do
+        location.menu = lib.registerContext({
+            id = tostring(location.name) .. '_menu',
+            title = location.name .. " Rental",
+            options = getcars(location),
+        })
+    end
+    peds()
+end
